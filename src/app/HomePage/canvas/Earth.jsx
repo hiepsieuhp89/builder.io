ef0daf28b46b1e1fdc56b45c7b87cd406e89ccf5
clintonweb <clinton-web@okvip.com>
2024-05-08 03:24:45 +0700

    t

---
+++ src/app/HomePage/canvas/Earth.jsx
@@ -0,0 +1,114 @@
+"use client";
+import * as THREE from 'three'
+import React, { Suspense, useMemo, useRef } from "react";
+import { Canvas, useFrame } from "@react-three/fiber";
+import {
+  Line,
+  OrbitControls,
+  Preload,
+  Trail,
+  useGLTF,
+} from "@react-three/drei";
+import { EffectComposer, Bloom } from "@react-three/postprocessing";
+
+import CanvasLoader from "../Loader";
+
+const Earth = () => {
+  const earth = useGLTF("./planet/scene.gltf");
+  const points = useMemo(() => new THREE.EllipseCurve(0, 0, 3, 1.15, 0, 2 * Math.PI, false, 0).getPoints(100), [])
+  return (
+    <group>
+      {/* <Line worldUnits points={points} color="turquoise" lineWidth={0.3} />
+      <Line
+        worldUnits
+        points={points}
+        color="turquoise"
+        lineWidth={0.3}
+        rotation={[0, 0, 1]}
+      />
+      <Line
+        worldUnits
+        points={points}
+        color="turquoise"
+        lineWidth={0.3}
+        rotation={[0, 0, -1]}
+      />
+      <Electron position={[0, 0, 0.5]} speed={6} />
+      <Electron
+        position={[0, 0, 0.5]}
+        rotation={[0, 0, Math.PI / 3]}
+        speed={6.5}
+      />
+      <Electron
+        position={[0, 0, 0.5]}
+        rotation={[0, 0, -Math.PI / 3]}
+        speed={7}
+      /> */}
+      <primitive
+        object={earth.scene}
+        scale={2.5}
+        position-y={0}
+        rotation-y={0}
+      />
+    </group>
+  );
+};
+function Electron({ radius = 2.75, speed = 6, ...props }) {
+  const ref = useRef();
+  useFrame((state) => {
+    const t = state.clock.getElapsedTime() * speed;
+    ref.current.position.set(
+      Math.sin(t) * radius,
+      (Math.cos(t) * radius * Math.atan(t)) / Math.PI / 1.25,
+      0,
+    );
+  });
+  return (
+    <group {...props}>
+      <Trail
+        local
+        width={5}
+        length={6}
+        color={new THREE.Color(2, 1, 10)}
+        attenuation={(t) => t * t}
+      >
+        <mesh ref={ref}>
+          <sphereGeometry args={[0.25]} />
+          <meshBasicMaterial color={[10, 1, 10]} toneMapped={false} />
+        </mesh>
+      </Trail>
+    </group>
+  );
+}
+const EarthCanvas = () => {
+  return (
+    <Canvas
+      shadows
+      frameloop="demand"
+      dpr={[1, 2]}
+      gl={{ preserveDrawingBuffer: true }}
+      camera={{
+        fov: 45,
+        near: 0.1,
+        far: 200,
+        position: [-4, 3, 6],
+      }}
+    >
+      <Suspense fallback={<CanvasLoader />}>
+        <OrbitControls
+          autoRotate
+          enableZoom={false}
+          maxPolarAngle={Math.PI / 2}
+          minPolarAngle={Math.PI / 2}
+        />
+        <Earth />
+        <EffectComposer>
+          <Bloom mipmapBlur luminanceThreshold={1} radius={0.7} />
+        </EffectComposer>
+        <Preload all />
+      </Suspense>
+    </Canvas>
+  );
+};
+
+export default EarthCanvas;
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,10 +4,26 @@ module.exports = {
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/**/*.{js,jsx}",
   ],
   theme: {
     extend: {
+      colors: {
+        primary: "rgb(16 7 0)",
+        secondary: "rgb(243 213 201)",
+        tertiary: "rgb(59 22 0)",
+        "black-100": "rgb(65 34 20)",
+        "black-200": "rgb(50 15 0)",
+        "white-100": "#f3f3f3",
+      },
+      boxShadow: {
+        card: "0px 35px 120px -15px #35211e",
+      },
+      screens: {
+        xs: "450px",
+      },
       backgroundImage: {
+        "hero-pattern": "url('/herobg.png')",
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
