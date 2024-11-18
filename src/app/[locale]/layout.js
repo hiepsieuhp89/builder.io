505af5251f8cfbb02574865fa1009bf81319f4fb
PhamNguyen2608 <pvnbmt2608@gmail.com>
2024-11-19 01:46:33 +0700

    implement google ads

--- src/app/[locale]/layout.js
+++ src/app/[locale]/layout.js
@@ -6,14 +6,18 @@ import { getMessages, unstable_setRequestLocale } from "next-intl/server";
 const inter = Inter({ subsets: ["latin"] });
 
 export default async function RootLayout({ children, params: { locale } }) {
-  // Providing all messages to the client
-  // side is the easiest way to get started
   const messages = await getMessages();
-  // Enable static rendering
   unstable_setRequestLocale(locale);
 
   return (
     <html lang={locale}>
+      <head>
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1208870163572573"
+          crossOrigin="anonymous"
+        />
+      </head>
       <body className={inter.className}>
         <NextIntlClientProvider messages={messages}>
           {children}
